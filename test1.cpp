61e416e112864155b994317733799cf80a6a3183
Kuerty <kamcwigda@gmail.com>
2017-05-25 15:55:24 +0200

    porzadki

--- test1.cpp
+++ test1.cpp
@@ -1,24 +1,33 @@
 #include <opencv2\opencv.hpp>
 #include <opencv/highgui.h>
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
 
 using namespace cv;
+using namespace std;
 
 int main() {
 
-	Mat image;
+	Mat image,image2;
 
-	VideoCapture cap;
-	cap.open(0);
+	image = imread("Hydrangeas.jpg", 1); //œcie¿ka obrazu
 
-	namedWindow("window", 1);
+	vector<Mat> channels;
+	cvtColor(image, image, COLOR_BGR2YCrCb); 
+	
+	split(image, channels);
+	equalizeHist(channels[0], channels[0]); //wyrównuje histogram sk³adowej Y (sk³adowa luminacji)
+	merge(channels, image2);
 
-	while (10) {
+	cvtColor(image2, image2, CV_YCrCb2BGR);
 
-		cap >> image;
-		
-		imshow("window", image);
-		waitKey(33);
 
-	}
+	namedWindow("window", CV_WINDOW_AUTOSIZE);
+	namedWindow("window2", CV_WINDOW_AUTOSIZE);
 
+
+	imshow("window2", image2);
+	waitKey(0);
+
+	return 0;
 }
