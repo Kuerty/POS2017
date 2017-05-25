08aecca6fa9d5836f8be461158587dfd4091e47d
Kuerty <kamcwigda@gmail.com>
2017-05-25 20:32:00 +0200

    dodany header i definicje funkcji, w mainie tylko wywolania

--- headerKamila.h
+++ headerKamila.h
@@ -8,7 +8,7 @@
 
 using namespace std;
 
-void przetworzobraz(const string nazwa_obrazu);
+void przetworzobraz(const string nazwa_obrazu); //przetwarza obraz metod¹ uœredniania jasnoœci
 
 
 #endif // !headerKamila_h
