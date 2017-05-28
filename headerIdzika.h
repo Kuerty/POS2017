101206c8d0ad22afb4e190f828a934230552fcec
idzik15 <idzik15@wp.pl>
2017-05-28 21:46:39 +0200

    ver 1

---
+++ headerIdzika.h
@@ -0,0 +1,13 @@
+#ifndef headerIdzika_h
+#define headerIdzika_h
+
+
+#include "stdafx.h"
+#include <iostream>
+#include <windows.h>
+#include<conio.h>
+#include<stdio.h>
+
+using namespace std;
+
+#endif 
